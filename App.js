581cd8f545b8dd43f51e5bb063cfa1ff89a8d17b
dengxinjuan <dengxinj@gmail.com>
2022-05-22 21:06:12 -0700

    add onesignal service

--- App.js
+++ App.js
@@ -11,6 +11,36 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import MainNavigation from './components/MainNavigation';
 
+
+
+import OneSignal from 'react-native-onesignal';
+
+//OneSignal Init Code
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId("YOUR-ONESIGNAL-APP-ID");
+//END OneSignal Init Code
+
+//Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log("Prompt response:", response);
+});
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+  console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+  let notification = notificationReceivedEvent.getNotification();
+  console.log("notification: ", notification);
+  const data = notification.additionalData
+  console.log("additionalData: ", data);
+  // Complete with null means don't show a notification.
+  notificationReceivedEvent.complete(notification);
+});
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log("OneSignal: notification opened:", notification);
+});
+
 const App = () => {
   
  
